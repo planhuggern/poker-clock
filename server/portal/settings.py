f3522d20c8848c3189c143b36c5787210d5052b2
Espen Holtebu <espen.holtebu@gmail.com>
2026-02-28 23:29:55 +0100

    fix: legg til WhiteNoiseMiddleware i MIDDLEWARE

--- server/portal/settings.py
+++ server/portal/settings.py
@@ -80,6 +80,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.common.CommonMiddleware",
 ]
 
