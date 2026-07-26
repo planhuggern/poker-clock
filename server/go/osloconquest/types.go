ac08c409f14b8116c78d5ec6cc0d39a145e5d60f
Espen Holtebu <espen.holtebu@gmail.com>
2026-07-26 23:43:58 +0200

    typer i go

---
+++ server/go/osloconquest/types.go
@@ -0,0 +1,51 @@
+package osloconquest
+
+type PlayerID string
+type MapNodeID string
+type Phase string
+
+const (
+	PhaseWaiting  Phase = "waiting"
+	PhaseSetup    Phase = "setup"
+	PhasePlaying  Phase = "playing"
+	PhaseGameOver Phase = "finished"
+)
+
+type Player struct {
+	ID   PlayerID
+	Name string
+	Color string
+	ColorName string
+	IsBot bool
+	Position *MapNodeID
+	MovesRemaining int
+	DiceRoll *int
+	ValidMoves []MapNodeID
+	SetupConfirmed bool
+	Money int
+	Units int
+	NextCheckpoint *MapNodeID
+}
+
+type MapNode struct {
+	ID    MapNodeID
+	Owner *PlayerID
+	Units int
+}
+
+type LogEntry struct {
+	Message string
+	Type	string
+	Time    string
+}
+
+type Room struct {
+	ID          string
+	Players     []Player
+	Territories map[MapNodeID]MapNode
+	Phase       Phase
+	Started     bool
+	ActivePlayer *PlayerID
+	Winner      *PlayerID
+	Log []LogEntry
+}
