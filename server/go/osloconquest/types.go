0bbb31bfed5e22fb035e9cbbd9cbea123121c594
Espen Holtebu <espen.holtebu@gmail.com>
2026-07-31 22:31:06 +0200

    clone room

--- server/go/osloconquest/types.go
+++ server/go/osloconquest/types.go
@@ -1,5 +1,10 @@
 package osloconquest
 
+import (
+	"maps"
+	"slices"
+)
+
 type PlayerID string
 type MapNodeID string
 type Phase string
@@ -29,6 +34,26 @@ type Player struct {
 	NextCheckpoint *MapNodeID
 }
 
+func (player Player) Clone() Player {
+	clonedPlayer := player
+	if player.Position != nil {
+		position := *player.Position
+		clonedPlayer.Position = &position
+	}
+	if player.DiceRoll != nil {
+		diceRoll := *player.DiceRoll
+		clonedPlayer.DiceRoll = &diceRoll
+	}
+	if player.NextCheckpoint != nil {
+		nextCheckpoint := *player.NextCheckpoint
+		clonedPlayer.NextCheckpoint = &nextCheckpoint
+	}
+	if player.ValidMoves != nil {
+		clonedPlayer.ValidMoves = slices.Clone(player.ValidMoves)
+	}
+	return clonedPlayer
+}
+
 type MapNode struct {
 	ID    MapNodeID
 	Owner *PlayerID
@@ -50,6 +75,13 @@ type Room struct {
 	ActivePlayerID *PlayerID
 	WinnerID       *PlayerID
 	Log            []LogEntry
+}
+
+func (room Room) Clone() Room {
+	room.Players = slices.Clone(room.Players)
+	room.Territories = maps.Clone(room.Territories)
+	room.Log = slices.Clone(room.Log)
+	return room
 }
 
 type RoomInfo struct {
